770fe46b362f2bb40ba107e58c4af4b39c276004
25gabriel-cmyk <134001939+25gabriel-cmyk@users.noreply.github.com>
2023-07-04 03:49:22 +0700

    Update main.py
    add interactive input password command

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import subprocess
+import os
+import getpass
 
 
 def run_command(command):
-  """Run a command in the terminal."""
-  subprocess.run(command, shell=True)
+    """Run a command in the terminal."""
+    subprocess.run(command, shell=True)
 
 
 if __name__ == "__main__":
-  # Create a virtual environment
-  run_command("python3 -m venv .venv")
+    # Create a virtual environment
+    run_command("python3 -m venv .venv")
 
-  # Activate the virtual environment
-  run_command("source .venv/bin/activate")
+    # Activate the virtual environment
+    activate_command = os.path.join(".venv", "bin", "activate")
+    run_command(f"source {activate_command}")
 
-  # Install 'tgcf' package
-  run_command("pip install tgcf")
+    # Install 'tgcf' package
+    run_command("pip install tgcf")
 
-  # Check 'tgcf' version
-  run_command("tgcf --version")
+    # Check 'tgcf' version
+    run_command("tgcf --version")
 
-  # Run TGCF Web
-  run_command("tgcf-web")
+    # Prompt for password
+    password = getpass.getpass("Enter your password: ")
+    command = f'echo "PASSWORD={password}" >> .env'
+    os.system(command)
+
+    # Run TGCF Web
+    run_command("tgcf-web")
